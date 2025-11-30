71c8f0af95d512fb45b4c25ba62d1dd791e00727
Nicholas2208 <nikolaj.belyakov.84@mail.ru>
2025-11-30 11:15:45 +0300

    Add Entity Graph

--- src/main/java/com/nwhite/urlshortener/domain/repository/ShortUrlRepository.java
+++ src/main/java/com/nwhite/urlshortener/domain/repository/ShortUrlRepository.java
@@ -1,6 +1,7 @@
 package com.nwhite.urlshortener.domain.repository;
 
 import com.nwhite.urlshortener.domain.entity.ShortUrl;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,5 +9,6 @@ import java.util.List;
 
 public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
     @Query("select su from ShortUrl su where su.isPrivate = false order by su.createdAt desc")
+    @EntityGraph(attributePaths = {"createdBy"})
     List<ShortUrl> findPublicShortUrls();
 }
